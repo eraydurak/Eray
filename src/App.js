655b8d6dbade4184b44a3fae74cf8581564df440
Eray Durak <eraydurak1@outlook.com>
2021-04-11 23:23:35 +0300

    Added contact page to main page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.scss";
 import Nav from "./components/Nav";
 import MainContent from "./components/MainContent";
+import Contact from "./components/Contact";
 
 const App = () => {
   return (
     <div className="App">
       <Nav />
       <MainContent />
+      <Contact />
     </div>
   );
 };
